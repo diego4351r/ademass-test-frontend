e19157e1242afbe46f589dd8ffafde82969d8af0
Diego <75797588+diego4351r@users.noreply.github.com>
2024-04-07 16:32:55 +0200

    scss folders and configuration. typographies added

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "src/assets/styles/abstracts/mixins/index" as *;
+          @use "src/assets/styles/abstracts/functions/index" as *;
+          @use "src/assets/styles/abstracts/variables/colors" as *;
+          `,
+      },
+    },
+  },
+});
